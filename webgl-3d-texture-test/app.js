bfe30bff8798cc8295ae3f508515d64c74e17f3e
Daniel Englisch <xeroaustria@hotmail.com>
2017-10-12 18:20:29 +0200

    added custom 3d model / texture test

--- webgl-3d-texture-test/app.js
+++ webgl-3d-texture-test/app.js
@@ -1,22 +1,3 @@
-function readTextFile(file)
-{
-	var rawFile = new XMLHttpRequest();
-	var allText = "";
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                allText = rawFile.responseText;
-            }
-        }
-    }
-	rawFile.send(null);
-	return allText;
-}
-
 
 var InitDemo = function(){
 
