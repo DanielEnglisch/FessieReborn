9f3674b985af5d3a3c0b1dd5b62f3a63012e1458
Daniel Englisch <xeroaustria@hotmail.com>
2017-11-03 14:02:19 +0100

    updated to foreach loops

--- javascript/main.js
+++ javascript/main.js
@@ -2,32 +2,30 @@ var context = null;
 var planets = new Array();
 var canvas = document.getElementById("screen");
 var zoomFactor = 80;
-var time  = 0;
-var deltaT =1e-6;
+var time = 0;
+var deltaT = 1e-3;
 
-
-function Planet(x, y, mass) {
+function Planet(x, y, mass,vx,vy) {
     this.x = x;
     this.y = y;
-    this.vx = 0;
-    this.vy = 0;
+    this.vx = vx;
+    this.vy = ,vy;
     this.ax = 0;
     this.ay = 0;
     this.mass = mass;
 
-    this.getDeltaX = function(o){
+    this.getDeltaX = function (o) {
         return o.x - this.x;
     };
 
-    this.getDeltaY = function(o){
-         return o.y - this.y;
+    this.getDeltaY = function (o) {
+        return o.y - this.y;
     };
 
-    this.getDistanceTo = function(o){
-         return Math.sqrt(
-             Math.pow(this.getDeltaX(o), 2)
-             +
-             Math.pow(this.getDeltaY(o), 2));
+    this.getDistanceTo = function (o) {
+        return Math.sqrt(
+            Math.pow(this.getDeltaX(o), 2) +
+            Math.pow(this.getDeltaY(o), 2));
     };
 };
 
@@ -36,11 +34,8 @@ var main = function () {
     addEvents();
     initCanvas();
 
-    var p1 = new Planet(0, -20000, 3e24);    
-    var p2 = new Planet(0, 20000, 3e24);    
-    
-    p1.vx = 20000;
-    p2.vx = -20000;
+    var p1 = new Planet(0, -20000, 3e24,20000,0);
+    var p2 = new Planet(0, 20000, 3e24,-20000,0);
 
     planets.push(p1);
     planets.push(p2);
@@ -55,7 +50,7 @@ var main = function () {
 
 var addEvents = function () {
     window.addEventListener("click", function (e) {
-        planets.push(new Planet((e.x-(window.innerWidth/2))*zoomFactor, (e.y-(window.innerHeight/2))*zoomFactor, 6*Math.pow(10,24)));
+        planets.push(new Planet((e.x - (window.innerWidth / 2)) * zoomFactor, (e.y - (window.innerHeight / 2)) * zoomFactor, 6 * Math.pow(10, 24)));
     });
 
     window.addEventListener("resize", function (e) {
@@ -68,49 +63,45 @@ var update = function () {
 
     //console.info(planets.length);
 
-    for (var i in planets) {
-        var p = planets[i];
-       
+    planets.forEach(function (p) {
 
-            var G = 6.67384e-11;
-            var rExponent = 3;
-    
-            // #1:Beschleunigung-Schleife:
-            for (var i in planets) {
-                var o1 = planets[i];
+        var G = 6.67384e-11;
+        var rExponent = 3;
 
-                var ax = 0.0;
-                var ay = 0.0;
-    
-                for (var j in planets) {
-                    var o2 = planets[j];                    
-                    if (o1 != o2) {
-    
-                        var deltaX = o1.getDeltaX(o2);
-                        var deltaY = o1.getDeltaY(o2);
-                        var r = o1.getDistanceTo(o2);
-    
-                        ax += (G * o2.mass * deltaX) / Math.pow(r, rExponent);
-                        ay += (G * o2.mass * deltaY) / Math.pow(r, rExponent);
-                    }
+        // #1:Beschleunigung-Schleife:
+        planets.forEach(function (o1) {
+
+            var ax = 0.0;
+            var ay = 0.0;
+
+            planets.forEach(function (o2) {
+
+                if (o1 !== o2) {
+
+                    var deltaX = o1.getDeltaX(o2);
+                    var deltaY = o1.getDeltaY(o2);
+                    var r = o1.getDistanceTo(o2);
+
+                    ax += (G * o2.mass * deltaX) / Math.pow(r, rExponent);
+                    ay += (G * o2.mass * deltaY) / Math.pow(r, rExponent);
                 }
-                o1.ax = ax;
-                o1.ay = ay;
-            }
-    
-            for (var k in planets) {
-                var o = planets[k];
-                o.vx += deltaT * o.ax;
-                o.vy += deltaT * o.ay;
-            }
-    
-            for (var l in planets) {
-                var o = planets[l];
-                o.x += (deltaT * o.vx) + (0.5 * o.ax * Math.pow(deltaT, 2));
-                o.y += (deltaT * o.vy) + (0.5 * o.ay * Math.pow(deltaT, 2));
-            }
-        }
-    time+=deltaT;
+            });
+            o1.ax = ax;
+            o1.ay = ay;
+        });
+
+        planets.forEach(function (o) {
+            o.vx += deltaT * o.ax;
+            o.vy += deltaT * o.ay;
+        });
+
+        planets.forEach(function (o) {
+            o.x += (deltaT * o.vx) + (0.5 * o.ax * Math.pow(deltaT, 2));
+            o.y += (deltaT * o.vy) + (0.5 * o.ay * Math.pow(deltaT, 2));
+        });
+    });
+
+    time += deltaT;
 
 };
 
@@ -119,7 +110,7 @@ var initCanvas = function () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     context = canvas.getContext("2d");
-    context.font = "15px Arial";    
+    context.font = "15px Arial";
 };
 
 var redraw = function () {
@@ -127,13 +118,12 @@ var redraw = function () {
     for (var i in planets) {
         var p = planets[i];
         context.beginPath();
-        context.arc((p.x/zoomFactor)+(window.innerWidth/2), (p.y/zoomFactor)+(window.innerHeight/2), 10, 0, 2 * Math.PI, false);
+        context.arc((p.x / zoomFactor) + (window.innerWidth / 2), (p.y / zoomFactor) + (window.innerHeight / 2), 10, 0, 2 * Math.PI, false);
         context.fillStyle = 'black';
         context.fill();
         context.stroke();
     }
-    context.fillText("Time = " + time + "s" ,10,15); 
-    context.fillText("DeltaT = " + deltaT + "s" ,10,15+15); 
-    
-};
+    context.fillText("Time = " + time + "s", 10, 15);
+    context.fillText("DeltaT = " + deltaT + "s", 10, 15 + 15);
 
+};
