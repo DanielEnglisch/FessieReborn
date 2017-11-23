802144138ed15b665a886ba409895decd689ae0e
Daniel Englisch <xeroaustria@hotmail.com>
2017-11-23 20:05:58 +0100

    added rock interaction

--- javascript/2D Game Test/main.js
+++ javascript/2D Game Test/main.js
@@ -1,55 +1,69 @@
 var canvas = document.getElementById("screen");
 var context = null;
 
+var rocks = new Array();
 
-var Vec = function(x, y) {
+var Vec = function (x, y) {
     this.x = x;
     this.y = y;
 };
 
-function Player (pos) {
+function Player(pos) {
     this.pos = pos;
 }
 
-var toBlockPos = function(pos){
+var toBlockPos = function (pos) {
     return new Vec(pos.x | 0, pos.y | 0);
 }
 
-function round(num) {    
-    return +(Math.round(num + "e+2")  + "e-2");
+function round(num) {
+    return +(Math.round(num + "e+2") + "e-2");
 }
 
-function Rock (pos) {
+function Rock(pos) {
     this.blockPos = pos;
-    this.pos = new Vec(pos.x,pos.y);
-    this.update = function(){
-        if(world[this.blockPos.x][this.blockPos.y +1 ] == 0){
-            this.pos.y+=0.1;
-            this.pos.y =  round(this.pos.y);
+    this.pos = new Vec(pos.x, pos.y);
+    this.update = function () {
+        if (world[this.blockPos.x][this.blockPos.y + 1] == 0) {
+            this.pos.y += 0.1;
+            this.pos.y = round(this.pos.y);
         }
-        this.blockPos = toBlockPos(this.pos);    
+        this.blockPos = toBlockPos(this.pos);
     }
 }
 
-var player = new Player(new Vec(1,1));
-var rock = new Rock(new Vec(3,3));
+var player = null;
 
 var world = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 2, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
-    [1, 0, 0, 0, 1, 1, 1, 0, 0, 1],
+    [1, 0, 0, 3, 1, 1, 1, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 3, 1, 1, 1, 0, 1],
     [1, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 3, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 4, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
+
+var initWorld = function () {
+    for (var y = 0; y < world.length; y++) {
+        for (var x = 0; x < world.length; x++) {
+
+            if (world[x][y] == 2) {
+                player = new Player(new Vec(x, y));
+            } else if (world[x][y] == 3) {
+                rocks.push(new Rock(new Vec(x, y)));
+            }
+        }
+    }
+}
 
 var main = function () {
     addEvents();
     initCanvas();
+    initWorld();
     loop();
 };
 
@@ -59,27 +73,43 @@ var loop = function () {
     requestAnimationFrame(loop);
 }
 
-var moveRock = function(dx,dy){
-    if(world[rock.pos.x + dx][rock.pos.y +dy] == 1)
+var moveRock = function (rock, dx, dy) {
+    var succ = true;
+    if (world[rock.pos.x + dx][rock.pos.y + dy] == 1)
         return false;
-    rock.pos.x+=dx;
-    rock.pos.y+=dy;
+
+    rocks.forEach(function (r) {
+        if (rock.pos.x + dx == r.pos.x && rock.pos.y + dy == r.pos.y) {
+            succ = false;
+        }
+    });
+
+    if (!succ)
+        return false;
+
+    rock.pos.x += dx;
+    rock.pos.y += dy;
     return true;
 }
 
-
-var movePlayer = function(dx,dy){
-
-    if(world[player.pos.x + dx][player.pos.y +dy] == 1)
+var movePlayer = function (dx, dy) {
+    var success = true;
+    if (world[player.pos.x + dx][player.pos.y + dy] == 1)
         return;
-    else if(player.pos.x +dx == rock.pos.x && player.pos.y + dy == rock.pos.y){
-        if(!moveRock(dx,dy))
-            return;
+    else {
+        rocks.forEach(function (rock) {
+            if (player.pos.x + dx == rock.pos.x && player.pos.y + dy == rock.pos.y) {
+                if (!moveRock(rock, dx, dy))
+                    success = false;
+            }
+        });
+
     }
-    
-    player.pos.x+=dx;
-    player.pos.y+=dy;
-    
+    if (!success)
+        return;
+    console.log("Moving");
+    player.pos.x += dx;
+    player.pos.y += dy;
 }
 
 var addEvents = function () {
@@ -95,23 +125,26 @@ var addEvents = function () {
 
         if (e.keyCode == '38') {
             console.log("UP ARROW");
-            movePlayer(0,-1);
+            movePlayer(0, -1);
         } else if (e.keyCode == '40') {
             console.log("DOWN ARROW");
-            movePlayer(0,+1);
+            movePlayer(0, +1);
         } else if (e.keyCode == '37') {
             console.log("LEFT ARROW");
-            movePlayer(-1,0);
+            movePlayer(-1, 0);
         } else if (e.keyCode == '39') {
             console.log("RIGHT ARROW");
-            movePlayer(1,0);
-            
+            movePlayer(1, 0);
+
         }
     };
 };
 
 var update = function () {
-    rock.update();
+
+    rocks.forEach(function (rock) {
+        rock.update();
+    });
 };
 
 var initCanvas = function () {
@@ -130,12 +163,12 @@ var redraw = function () {
     for (var y = 0; y < world.length; y++) {
         for (var x = 0; x < world.length; x++) {
 
+            // Walls
             if (world[x][y] == 1) {
                 context.fillStyle = "#FF0000";
-            } else if (world[x][y] == 0) {
+            } else {
                 context.fillStyle = "#00FFFF";
-            } else
-                context.fillStyle = "#FF00FF";
+            }
 
             context.fillRect(x * scale, y * scale, scale, scale);
             context.stroke();
@@ -144,11 +177,14 @@ var redraw = function () {
 
     // Draw Player
     context.fillStyle = "#0000FF";
-    context.fillRect(player.pos.x * scale, player.pos.y* scale, scale, scale);
+    context.fillRect(player.pos.x * scale, player.pos.y * scale, scale, scale);
     context.stroke();
 
     // Draw Rocks
     context.fillStyle = "#FFFF00";
-    context.fillRect(rock.pos.x * scale, rock.pos.y* scale, scale, scale);
-    context.stroke();
+    rocks.forEach(function (rock) {
+        context.fillRect(rock.pos.x * scale, rock.pos.y * scale, scale, scale);
+        context.stroke();
+    });
+
 };
