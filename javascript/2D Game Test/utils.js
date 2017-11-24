f263ee9aa6cc3baefd9b525532107a124d710581
Developer <developer@localhost>
2017-11-24 09:58:57 +0100

    #4: Split up into multiple files

---
+++ javascript/2D Game Test/utils.js
@@ -0,0 +1,50 @@
+function transpose(a) {
+  
+      // Calculate the width and height of the Array
+      var w = a.length || 0;
+      var h = a[0] instanceof Array ? a[0].length : 0;
+  
+      // In case it is a zero matrix, no transpose routine needed.
+      if (h === 0 || w === 0) {
+          return [];
+      }
+  
+      /**
+       * @var {Number} i Counter
+       * @var {Number} j Counter
+       * @var {Array} t Transposed data is stored in this array.
+       */
+      var i, j, t = [];
+  
+      // Loop through every item in the outer array (height)
+      for (i = 0; i < h; i++) {
+  
+          // Insert a new row (array)
+          t[i] = [];
+  
+          // Loop through every item per item in outer array (width)
+          for (j = 0; j < w; j++) {
+  
+              // Save transposed data.
+              t[i][j] = a[j][i];
+          }
+      }
+  
+      return t;
+  }
+
+  
+function readTextFile(file) {
+  var allText = null;
+  var rawFile = new XMLHttpRequest();
+  rawFile.open("GET", file, false);
+  rawFile.onreadystatechange = function () {
+      if (rawFile.readyState === 4) {
+          if (rawFile.status === 200 || rawFile.status == 0) {
+              allText = rawFile.responseText;
+          }
+      }
+  }
+  rawFile.send(null);
+  return allText;
+}
---
+++ javascript/2D Game Test/worldLoader.js
@@ -0,0 +1,45 @@
+
+var currentLevel = null;
+
+var reloadLevel= function(){
+  loadLevel(currentLevel);
+}
+
+var loadLevel=function(file){
+  // Reset everything:
+  currentLevel = file;
+  world = [];
+  player = null;
+  rocks = [];
+  // Load form file into world matrix
+  var txt = readTextFile(file);
+  var rows = txt.split('\n');
+  var numcol = 0;
+  for (var x = 0; x < rows.length - 1; x++) {
+      var lineArr = [];
+      for (var y = 0; y < rows[x].length - 1; y++) {
+          lineArr[y] = rows[x].charAt(y);
+          numcol = rows[x].length;
+      }
+      world[x] = lineArr;
+      numcol++;
+  }
+  console.log("Loaded level with size " + numcol + "x" + rows.length);
+
+  world = transpose(world);
+
+  // Exctracting Game Objects from matrix
+  for (var x = 0; x < world.length; x++) {
+      for (var y = 0; y < world[0].length; y++) {
+
+          if (world[x][y] == Block.PLAYER) {
+              player = new Player(new Vec(x, y));
+              world[x][y] = 0;
+          } else if (world[x][y] == Block.ROCK) {
+              rocks.push(new Rock(new Vec(x, y)));
+              world[x][y] = 0;
+
+          }
+      }
+  }
+}
