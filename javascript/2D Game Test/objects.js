083e074554ba2139008ddbeb29814c1e0ea7add0
Developer <developer@localhost>
2017-11-24 10:19:25 +0100

    #3: Added rock slipping

--- javascript/2D Game Test/objects.js
+++ javascript/2D Game Test/objects.js
@@ -1,54 +1,66 @@
-
 var Vec = function (x, y) {
-  this.x = x;
-  this.y = y;
+    this.x = x;
+    this.y = y;
 };
 
 function Player(pos) {
-  this.pos = pos;
+    this.pos = pos;
 }
 
 function Rock(pos) {
-  this.blockPos = pos;
-  this.pos = new Vec(pos.x, pos.y);
-  this.falling = false;
-  this.moveRock = function (dx, dy) {
+    this.blockPos = pos;
+    this.pos = new Vec(pos.x, pos.y);
+    this.falling = false;
+    this.moveRock = function (dx, dy) {
 
-      var succ = true;
+        var succ = true;
 
-      // When requested position is wall return
-      if (world[this.blockPos.x + dx][this.blockPos.y + dy] == Block.WALL)
-          return false;
-      // Check if requested position is rock
-      var myBlockPos = this.blockPos;
-      rocks.forEach(function (r) {
-          if (myBlockPos.x + dx == r.blockPos.x && myBlockPos.y + dy == r.blockPos.y) {
-              succ = false;
-          }
-      });
+        // When requested position is wall return
+        if (world[this.blockPos.x + dx][this.blockPos.y + dy] == Block.WALL)
+            return false;
+        // Check if requested position is rock
+        var myBlockPos = this.blockPos;
+        rocks.forEach(function (r) {
+            if (myBlockPos.x + dx == r.blockPos.x && myBlockPos.y + dy == r.blockPos.y) {
+                succ = false;
+            }
+        });
 
-      if (!succ)
-          return false;
+        if (!succ)
+            return false;
 
-      this.blockPos.x += dx;
-      this.blockPos.y += dy;
-      this.pos = this.blockPos;
-      return true;
-  }
+        this.blockPos.x += dx;
+        this.blockPos.y += dy;
+        this.pos = this.blockPos;
+        return true;
+    }
 
-  this.update = function () {
-      // If falling and not at end position
-      if (this.falling && this.pos.y < this.blockPos.y) {
-          this.pos.y += 0.1;
-          this.pos.y = Math.round(this.pos.y * 100) / 100
-      } else if (this.pos.y == this.blockPos.y) {
-          this.falling = false;
-      }
+    this.update = function () {
 
-      if (!this.falling && isAir(this.blockPos.x, this.blockPos.y + 1)) {
-          console.log("Falling");
-          this.falling = true;
-          this.blockPos = new Vec(this.blockPos.x, this.blockPos.y + 1);
-      }
-  }
+        // If rock is on other rock -> slip to side if possible
+        if (!this.falling && isRock(this.blockPos.x, this.blockPos.y + 1)) {
+            // If Right Is air
+            if (isAir(this.blockPos.x + 1, this.blockPos.y + 1)) {
+                this.moveRock(1,0);
+
+            // If Left Is air
+            } else if (isAir(this.blockPos.x - 1, this.blockPos.y + 1)) {
+                this.moveRock(-1,0);
+            }
+        }
+
+        // If falling and not at end position
+        if (this.falling && this.pos.y < this.blockPos.y) {
+            this.pos.y += 0.1;
+            this.pos.y = Math.round(this.pos.y * 100) / 100
+        } else if (this.pos.y == this.blockPos.y) {
+            this.falling = false;
+        }
+
+        if (!this.falling && isAir(this.blockPos.x, this.blockPos.y + 1)) {
+            console.log("Falling");
+            this.falling = true;
+            this.blockPos = new Vec(this.blockPos.x, this.blockPos.y + 1);
+        }
+    }
 }
