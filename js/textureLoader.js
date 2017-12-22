6874795be6aa837d098a14108c8227b8e0ca1833
Daniel Englisch <xeroaustria@hotmail.com>
2017-12-22 16:10:55 +0100

    removed unused images

--- js/textureLoader.js
+++ js/textureLoader.js
@@ -3,10 +3,6 @@ function TexturesBundle() {
   this.allTexturesLoading = false;
 
   this.player_neutral = new Image();
-  this.player_up = new Image();
-  this.player_down = new Image();
-  this.player_left = new Image();
-  this.player_right = new Image();
 
   this.dumpster1 = new Image();
   this.dumpster2 = new Image();
@@ -151,10 +147,6 @@ function TexturesBundle() {
   this.load = function (dir, callback) {
     this.callback = callback;
     this.player_neutral = this.blockloadImage(dir + "p.png");
-    this.player_up = this.blockloadImage(dir + "p_up.png");
-    this.player_down = this.blockloadImage(dir + "p_down.png");
-    this.player_left = this.blockloadImage(dir + "p_left.png");
-    this.player_right = this.blockloadImage(dir + "p_right.png");
 
     this.wall = this.blockloadImage(dir + "wall.png");
     this.exit_closed = this.blockloadImage(dir + "exit_closed.png");
