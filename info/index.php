fe10a89e5b8ceabd1618e9bc0563707188559e2e
DanielEnglisch <xeroaustria@hotmail.com>
2017-12-30 21:10:39 +0100

    closes #99 added simple info

--- info/index.php
+++ info/index.php
@@ -5,6 +5,16 @@
   <meta charset="utf8">
   <title>Fessie reborn</title>
   <link rel="stylesheet" type="text/css" href="../css/game.css">
+  <style>
+    h1{
+      padding-left: 10px;
+    }
+    p{
+      font-size: 12px;
+      padding-left: 20px;
+      max-width: 50%;
+    }
+  </style>
 </head>
 
 <body>
@@ -15,6 +25,24 @@
     <a href="../editor/">Level Editor</a>
     <a href="../select/">Level Selection</a>
   </div>
+
+  <h1> Spielidee </h1>
+  <p>
+    “FESSIE räumt auf” ist ein Spiel ganz in der Tradition der Spielhallen-Art.
+    Der Spieler schlüpft in die Rolle des Drachen FESSIE. Seine Aufgabe ist es,
+    in vielen labyrinthartigen Levels diversen Müll aufzusammeln.
+    Um durch die Level zu kommen muss FESSIE so manchen Gegner austricksen
+    und verzwickte Hindernisse überwinden.
+    Dabei stehen ihm Bomben und die Fähigkeit Feuer zu speien zur Verfügung.
+  </p>
+  <h1> Steuerung </h1>
+  <p>
+    <b>Laufen</b>: Pfeiltasten<br>
+    <b>Feuerspeien</b>: [Umschalttaste]+ Pfeil links/rechts<br>
+    <b>Feld neben dem Spieler berühren, ohne es zu betreten</b>: [Strg] + Pfeil links/rechts<br>
+    <b>Bombe legen</b>: [Leertaste]<br>
+    <b>Aktuelles Level neustarten</b>: [R]
+  </p>
 
 </body>
 
